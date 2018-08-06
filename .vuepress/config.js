33800423cb23f00dfa5fcb190b3b4b8b4c5a5aad
PhEEP <philippealmoore@gmail.com>
2018-08-05 20:20:23 -0700

    Playing around with theming

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,6 +2,7 @@ module.exports = {
   title: "Pheep Devs",
   description: "Where Pheep writes about learning stuff",
   themeConfig: {
-    nav: [{ text: "Home", link: "/" }, { text: "WSL", link: "/WSL/" }]
+    nav: [{ text: "Home", link: "/" }, { text: "WSL", link: "/WSL/" }],
+    lastUpdated: "Last Updated" // string | boolean
   }
 };
